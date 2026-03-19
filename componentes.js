e94fb1651fe7452b1a6c3fc2dad14b133b6fdae6
corpus-teologico <rformigob@gmail.com>
2026-03-19 12:18:08 +0100

    Update componentes.js

--- componentes.js
+++ componentes.js
@@ -262,7 +262,7 @@ document.addEventListener("DOMContentLoaded", () => {
                             <a href="${rutaBase}estudios/la-iglesia-de-cristo.html" class="nav-link-full" data-path="la-iglesia-de-cristo.html">XXI. La Iglesia de Cristo</a>
                             <a href="${rutaBase}estudios/el-bautismo-biblico.html" class="nav-link-full" data-path="el-bautismo-biblico.html">XXII. La Mayordomía y el Trabajo</a>
                             <a href="${rutaBase}estudios/la-gran-comision.html" class="nav-link-full" data-path="la-gran-comision.html">XXIII. La Tentación</a>
-                            <a href="${rutaBase}estudios/el-adulterio.html" class="nav-link-full" data-path="el-adulterio.html">XXIII. La Tentación</a>
+                            <a href="${rutaBase}estudios/el-adulterio.html" class="nav-link-full" data-path="el-adulterio.html">XIV. El Adulterio</a>
                         </div>
                     </div>
                 </div>
